781f732802824b2efb889a5fe83b0d921f28fff3
Tobias <tobias.luderer@gmail.com>
2020-06-15 00:48:18 +0200

    updated dbdelete to delete ingredients

--- nomnom/management/commands/dbdelete.py
+++ nomnom/management/commands/dbdelete.py
@@ -1,22 +1,10 @@
 from django.core.management.base import BaseCommand, CommandError
 
-from accant.models import Account_Type_Template
-from accant.models import Account_Template
-from accant.models import Account_Type
-from accant.models import Account
-from accant.models import Area
-from accant.models import Transaction
-from accant.models import Transaction_Tag
+from nomnom.models import Ingredient
 
 
 class Command(BaseCommand):
     help = 'Creates mock data and fills database with it.'
 
     def handle(self, *args, **options):
-        Account_Type_Template.objects.all().delete
-        Account_Template.objects.all().delete
-        Account_Type.objects.all().delete
-        Account.objects.all().delete
-        Area.objects.all().delete
-        Transaction.objects.all().delete
-        Transaction_Tag.objects.all().delete
+        Ingredient.objects.all().delete

129e82ee8fd8e412d884be5c05d08045e320f9c8
Tobias <tobias.luderer@gmail.com>
2020-11-26 09:40:20 +0100

    Auto-Deployed Webapp

--- nomnom_server/settings.py
+++ nomnom_server/settings.py
@@ -21,6 +21,8 @@ else:
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'nomnom/static')
 
 # Quick-start development settings - unsuitable for production
@@ -154,6 +156,6 @@ DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = ''
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'nomnom/')]

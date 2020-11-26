75e3a4cc9565d49258787841645db954c4789ee2
Tobias <tobias.luderer@gmail.com>
2020-11-26 10:27:26 +0100

    revert pkg delete

--- nomnom_server/settings.py
+++ nomnom_server/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
     'oauth2_provider',

1588ab0fb813b3b1ed38144dc6fe49b043f98fa2
Ubuntu <pi4b@ubuntu.fritz.box>
2021-04-16 09:29:37 +0000

    removed legacy code

--- old/docker-entrypoint-initdb.d/init-user-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE nomnom_db;
-    CREATE USER nomnom WITH PASSWORD 'test';
-    ALTER DATABASE nomnom_db OWNER TO nomnom;
-EOSQL
